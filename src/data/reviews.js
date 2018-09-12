c2b671a812f97d0c65091a00a10d95003aa7555f
Ian Charters <ian@boot.rip>
2018-09-11 18:03:41 -0700

    Add `users` and `reviews` data.

---
+++ src/data/reviews.js
@@ -0,0 +1,57 @@
+export default [
+  // movie 1
+  {
+    id: 1,
+    userID: 1,
+    movieID: 1,
+    rating: 5,
+    content: 'This movie is totally awesome.',
+  },
+  {
+    id: 2,
+    userID: 2,
+    movieID: 1,
+    rating: 3,
+    content: 'This movie is alright.',
+  },
+
+  // movie 2
+  {
+    id: 3,
+    userID: 1,
+    movieID: 2,
+    rating: 1,
+    content: 'The director of this movie is a human potato.',
+  },
+  {
+    id: 4,
+    userID: 2,
+    movieID: 2,
+    rating: 4,
+    content: 'Purple drank is the best.',
+  },
+  {
+    id: 5,
+    userID: 3,
+    movieID: 2,
+    rating: 5,
+    content: "They don't think it be like it is, but it do.",
+  },
+
+  // movie 3
+  {
+    id: 6,
+    userID: 2,
+    movieID: 3,
+    rating: 5,
+    content:
+      "Boy howdy do I ever like country music.  How did I get here?  I'm not good with computer.",
+  },
+  {
+    id: 7,
+    userID: 3,
+    movieID: 3,
+    rating: 5,
+    content: 'Commenting so I can come back to this post later.',
+  },
+];
---
+++ src/data/users.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    username: 'izaaks',
+    name: 'Izaak Schroeder',
+    password: 'cheesesteak',
+  },
+  {
+    id: 2,
+    username: 'ianc',
+    name: 'Ian Charters',
+    password: 'y tho',
+  },
+];
